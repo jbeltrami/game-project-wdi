e6b93dec6f4d557b4099c612dafa5023c466ff22
jbeltrami <joao_beltrami@hotmail.com>
2017-04-12 19:52:53 -0400

    Adds check winner function
    Creates a function that checks for winner on the board.

--- assets/scripts/gameLogic.js
+++ assets/scripts/gameLogic.js
@@ -7,7 +7,7 @@ let turn = 'X'
 $('.col-xs-4').on('click', function () {
   if (this.textContent === '') {
     $(this).text(turn)
-    changeTurn()
+    checkWinner()
   } else {
     changeMessage('please, pick another square')
   }
@@ -30,36 +30,31 @@ const changeTurn = () => {
   } changeMessage('It is ' + turn + "'s turn now")
 }
 
+const checkWinner = function () {
+  if ($('.col-xs-4')[0].textContent === $('.col-xs-4')[1].textContent && $('.col-xs-4')[1].textContent === $('.col-xs-4')[2].textContent && $('.col-xs-4')[1].textContent !== '') {
+    return changeMessage(turn + ' won on the first row!')
+  } else if ($('.col-xs-4')[3].textContent === $('.col-xs-4')[4].textContent && $('.col-xs-4')[4].textContent === $('.col-xs-4')[5].textContent && $('.col-xs-4')[3].textContent !== '') {
+    return changeMessage(turn + ' won on the second row!')
+  } else if ($('.col-xs-4')[6].textContent === $('.col-xs-4')[7].textContent && $('.col-xs-4')[7].textContent === $('.col-xs-4')[8].textContent && $('.col-xs-4')[6].textContent !== '') {
+    return changeMessage(turn + ' won on the third row!')
+  } else if ($('.col-xs-4')[0].textContent === $('.col-xs-4')[3].textContent && $('.col-xs-4')[3].textContent === $('.col-xs-4')[6].textContent && $('.col-xs-4')[0].textContent !== '') {
+    return changeMessage(turn + ' won on the first column!')
+  } else if ($('.col-xs-4')[1].textContent === $('.col-xs-4')[4].textContent && $('.col-xs-4')[4].textContent === $('.col-xs-4')[7].textContent && $('.col-xs-4')[1].textContent !== '') {
+    return changeMessage(turn + ' won on the second column!')
+  } else if ($('.col-xs-4')[2].textContent === $('.col-xs-4')[5].textContent && $('.col-xs-4')[5].textContent === $('.col-xs-4')[8].textContent && $('.col-xs-4')[2].textContent !== '') {
+    return changeMessage(turn + ' won on the third column')
+  } else if ($('.col-xs-4')[0].textContent === $('.col-xs-4')[4].textContent && $('.col-xs-4')[4].textContent === $('.col-xs-4')[8].textContent && $('.col-xs-4')[0].textContent !== '') {
+    return changeMessage(turn + ' won on the right to left diagonal')
+  } else if ($('.col-xs-4')[2].textContent === $('.col-xs-4')[4].textContent && $('.col-xs-4')[4].textContent === $('.col-xs-4')[6].textContent && $('.col-xs-4')[2].textContent !== '') {
+    return changeMessage(turn + ' won on the left to right diagonal')
+  } else {
+    changeTurn()
+  }
+}
+
 module.exports = {
   changeMessage,
   startGame,
-  changeTurn
+  changeTurn,
+  checkWinner
 }
-
-//
-// $(() => {
-//   $('.data-id').on('click', nextMove())
-// })
-//
-
-// if (table[0] === table[1] && table[1] === table[2] && table[1] !== 0) {
-//   console.log('You won on the first row!')
-// } else if (table[3] === table[4] && table[4] === table[5] && table[3] !== 0) {
-//   console.log('You won on the second row!')
-// } else if (table[6] === table[7] && table[7] === table[8] && table[6] !== 0) {
-//   console.log('You won on the third row!')
-// } else if (table[0] === table[3] && table[3] === table[6] && table[0] !== 0) {
-//   console.log('You won on the first column!')
-// } else if (table[1] === table[4] && table[4] === table[7] && table[1] !== 0) {
-//   console.log('You won on the second column!')
-// } else if (table[2] === table[5] && table[5] === table[8] && table[2] !== 0) {
-//   console.log('You won on the third column')
-// } else if (table[0] === table[4] && table[4] === table[8] && table[0] !== 0) {
-//   console.log('You won on the right to left diagonal')
-// } else if (table[2] === table[4] && table[4] === table[6] && table[2] !== 0) {
-//   console.log('You won on the left to right diagonal')
-// } else {
-//   console.log('Game not finished yet')
-// }
-
-// $('.data-id').on('click', changeArray())
